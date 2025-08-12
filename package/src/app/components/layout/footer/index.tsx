960d0ff69493e4deade6e3f60e6f8983b79c1d2b
Get Nextjs Templates <nextjs@adminmart.com>
2025-08-12 17:57:16 +0530

    Update index.tsx

--- package/src/app/components/layout/footer/index.tsx
+++ package/src/app/components/layout/footer/index.tsx
@@ -26,7 +26,7 @@ const Footer = () => {
                 <div className="container">
                     <div className="flex flex-col sm:flex-row gap-2 items-center justify-between text-center sm:text-left">
                         <p>© <Link href={"/"} className="hover:text-primary">Docsta</Link>. All rights reserved Created by
-                            <Link href={"/https://getnextjstemplates.com/"} className="hover:text-primary"> Getnextjstemplates.com</Link></p>
+                            <Link href={"https://getnextjstemplates.com/"} className="hover:text-primary"> Getnextjstemplates.com</Link></p>
                         <div className="flex flex-wrap items-center row-gap-2 gap-x-5 justify-center">
                             {footerLink?.map((value: any, index: any) => {
                                 return (
